9e701050e046f7329f2cd6e027d98257c8bb8b18
Ruiming Kang <ruimingkang@outlook.com>
2023-07-15 21:53:44 +0800

    api

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
