cecc6148505861ead6fb3360d7099c5ef876f925
Ruiming Kang <ruimingkang@outlook.com>
2023-07-15 21:50:25 +0800

    commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify, render_template
+import os
+import sys
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+import constants
+
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+app = Flask(__name__)
+
+# Enable to save to disk & reuse the model (for repeated queries on the same data)
+PERSIST = False
+'''
+@app.route('/')
+def index():
+   return open('templates/index.html').read()
+'''
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    query = request.json['query']
+    chat_history = []
+    
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    else:
+        # loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
+        loader = DirectoryLoader("data/")
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+        else:
+            index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
+
+    #chat_history = []
+    result = chain({"question": query, "chat_history": chat_history})
+    answer = result['answer']
+
+    chat_history.append((query, answer))
+    return jsonify({'result': answer})
+
+if __name__ == '__main__':
+    app.run()
